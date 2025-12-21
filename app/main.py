00bf3ae0f604bd152c16c81998536287f5ddc0e3
Rafael-Jost <rafael2552005@gmail.com>
2025-12-21 19:44:08 -0300

    feat: Upload de arquivo Básico com FastAPI

---
+++ app/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, UploadFile
+from pydantic import BaseModel
+
+class ReceiptInfo(BaseModel):
+    filename: str = None
+    content: str = None
+    size: int = 0
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Scan2Spend"}
+
+@app.post("/upreceipt/", response_model=ReceiptInfo)
+async def upload_receipt(receipt: UploadFile):
+    file = await receipt.read()
+    file_content = file.decode('utf-8')
+    file_lenght = len(file)
+    return {"filename": receipt.filename, "size": file_lenght, "content": file_content}
