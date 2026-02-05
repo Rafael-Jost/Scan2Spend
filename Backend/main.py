74d2f775a418e24c6b298ba464507b1342edaab9
Rafael-Jost <163748830+Rafael-Jost@users.noreply.github.com>
2026-02-05 11:35:13 -0300

    feat: API agora extrai texto da imagem com OCR e retorna como "text"

--- Backend/main.py
+++ Backend/main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from pytesseract import pytesseract
+from PIL import Image
+import io
 
 class ReceiptInfo(BaseModel):
     filename: str = None
     content: str = None
     size: int = 0
+    text: str = "Nenhum texto extraído"
 
 app = FastAPI()
 
@@ -31,8 +35,12 @@ async def upload_receipt(receipt: UploadFile):
     file_bytes = await receipt.read()
     file_length = len(file_bytes)
 
+    image = Image.open(io.BytesIO(file_bytes))
+    text = pytesseract.image_to_string(image)
+
     return {
         "filename": receipt.filename,
         "size": file_length,
-        "content": receipt.content_type
+        "content": receipt.content_type,
+        "text": text
     }
