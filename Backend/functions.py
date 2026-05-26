e44a5bf25dc16c17b2355e838ba66d55ea45e3b4
Rafael-Jost <rafael2552005@gmail.com>
2026-05-25 23:08:47 -0300

    feat: implementa template HTML para envio de e-mail de redefinição de senha

--- Backend/functions.py
+++ Backend/functions.py
@@ -1,24 +1,30 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
 import os
 
 def enviar_redefinicao_senha(email_destino, token):
     email_sender = os.getenv("EMAIL_SENDER")
     email_password = os.getenv("EMAIL_PASSWORD")
 
-    msg = MIMEMultipart("alternative")
+    msg = MIMEMultipart("related")
     msg["Subject"] = "Redefinição de senha"
     msg["From"] = email_sender
     msg["To"] = email_destino
 
-    html = f"""
-    <p>Clique para redefinir sua senha:</p>
-    <a href="http://localhost:5173/reset-senha?token={token}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; border-radius: 5px;">
-        Redefinir Senha
-    </a>
-    """
+    template_path = './utils/email_redefinicao_senha.html'
+    with open(template_path, encoding="utf-8") as f:
+        html = f.read().replace("{{token}}", token)
+
     msg.attach(MIMEText(html, "html"))
+
+    logo_path = '../Frontend/src/assets/Scan2Spend_logo.png'
+    with open(logo_path, 'rb') as f:
+        img = MIMEImage(f.read())
+        img.add_header('Content-ID', '<logo_scan2spend>')
+        img.add_header('Content-Disposition', 'inline')
+        msg.attach(img)
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(email_sender, email_password)
